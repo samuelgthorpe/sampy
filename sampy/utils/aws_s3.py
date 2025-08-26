f6e05bc0615a5e24da1c4b6bab3206ef2980f62a
sam <samuelgthorpe@gmail.com>
2025-08-25 19:43:36 -0700

    fix: aws_s3 push folder and logging

--- sampy/utils/aws_s3.py
+++ sampy/utils/aws_s3.py
@@ -15,12 +15,7 @@ by Samuel Thorpe
 import os
 import boto3
 from logging import getLogger
-from sampy.utils import try_catch_log
-
-
-# # Globals
-# -----------------------------------------------------|
-AwsS3Exception = type('AwsS3Exception', (Exception,), {})
+from sampy.utils.logger import log_exceptions
 logger = getLogger(__name__)
 
 
@@ -39,7 +34,7 @@ class AwsS3:
         self._session = boto3.Session(**self._params)
         self._resource = boto3.resource('s3')
 
-    @try_catch_log(AwsS3Exception)
+    @log_exceptions()
     def get_file_in_path(self, s3_path, local_dir):
         """Pull specified s3 file to local dir.
 
@@ -60,7 +55,7 @@ class AwsS3:
 
         return output
 
-    @try_catch_log(AwsS3Exception)
+    @log_exceptions()
     def get_files_in_s3_directory(self, s3_dir, local_dir):
         """Pull specified s3 dir to local dir.
 
@@ -87,7 +82,7 @@ class AwsS3:
 
         return outputs
 
-    @try_catch_log(AwsS3Exception)
+    @log_exceptions()
     def push_files_to_s3(self, local_dir, bucket, s3_dir, **kwrgs):
         """Push specified local dir to s3.
 
@@ -107,7 +102,7 @@ class AwsS3:
 
             self.push_file_to_s3(f"{local_dir}/{file}", bucket, s3_dir)
 
-    @staticmethod
+    @log_exceptions()
     def _check_ignore(file, **kwrgs):
         """Return True if file should be ignored."""
         dss_chk = file == '.DS_Store'
@@ -115,7 +110,7 @@ class AwsS3:
 
         return any([dss_chk, ext_chk])
 
-    @try_catch_log(AwsS3Exception)
+    @log_exceptions()
     def push_file_to_s3(self, local_file, bucket, s3_dir):
         """Push specified local file to s3."""
         logger.info(f"Pushing {local_file} to S3 location: {bucket}/{s3_dir}")
@@ -123,3 +118,32 @@ class AwsS3:
         client = self._session.client('s3')
         local_dir, file = os.path.split(local_file)
         client.upload_file(local_file, bucket, f"{s3_dir}/{file}")
+
+    @log_exceptions()
+    def push_folder_to_s3(self, local_dir, bucket_name, prefix=""):
+        """
+        Recursively upload a local folder to S3, preserving subdir structure.
+
+        Parameters:
+        local_dir (str): Path to the local folder to upload.
+        bucket_name (str): Name of the S3 bucket.
+        prefix (str): Optional folder path in S3 to upload the contents into.
+        """
+        s3_client = boto3.client("s3")
+
+        # Walk through the local folder
+        for root, _, files in os.walk(local_dir):
+            for file_name in files:
+                local_pth = os.path.join(root, file_name)
+
+                # Create the relative path for the S3 object key
+                rel_pth = os.path.relpath(local_pth, local_dir)
+                s3_key = os.path.join(prefix, rel_pth).replace("\\", "/")
+                s3_url = f"s3://{bucket_name}/{s3_key}"
+
+                # Upload the file to S3
+                try:
+                    s3_client.upload_file(local_pth, bucket_name, s3_key)
+                    logger.info(f"Uploaded {local_pth} to {s3_url}")
+                except Exception as e:
+                    print(f"Failed to upload {local_pth}: {e}")
